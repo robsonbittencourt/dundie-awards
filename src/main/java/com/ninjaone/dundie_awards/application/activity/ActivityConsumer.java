de2d6b19f68f5f32d6a4f81e7929bee6ecbe9967
Robson Bittencourt <robson.luizv@gmail.com>
2025-05-04 14:13:13 -0300

    Add log configuration

--- src/main/java/com/ninjaone/dundie_awards/application/activity/ActivityConsumer.java
+++ src/main/java/com/ninjaone/dundie_awards/application/activity/ActivityConsumer.java
@@ -2,19 +2,23 @@ package com.ninjaone.dundie_awards.application.activity;
 
 import com.ninjaone.dundie_awards.infrastructure.config.RabbitConfig;
 import com.ninjaone.dundie_awards.infrastructure.repository.activity.Activity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ActivityConsumer {
 
+    private static final Logger log = LoggerFactory.getLogger(ActivityConsumer.class);
+
     @RabbitListener(queues = RabbitConfig.ACTIVITY_QUEUE)
     public void receive(Activity activity) {
-        System.out.println("Activity received: Event " + activity.getEvent() + " - Occurred at: " + activity.getOccuredAt());
+        log.info("Activity received: Event {} - Occurred at: {}", activity.getEvent(), activity.getOccuredAt());
     }
 
     @RabbitListener(queues = RabbitConfig.LOG_QUEUE)
     public void log(Activity activity) {
-        System.out.println("Log: Event " + activity.getEvent() + " - Occurred at: " + activity.getOccuredAt());
+        log.info("Log: Event {} - Occurred at: {}", activity.getEvent(), activity.getOccuredAt());
     }
 }
